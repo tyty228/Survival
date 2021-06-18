e7f1b50ebd76abf9ceb6fd2642750d95691b39cf
DraimCiDo <danilaorlov4@gmail.com>
2021-06-18 23:59:36 +0300

    Начало разработки Survival

---
+++ start.sh
@@ -0,0 +1,15 @@
+    #!/bin/sh
+         BINDIR=$(dirname "$(readlink -fn "$0")")
+         cd "/root/Survival/Survival-1.17"
+         while true
+         do
+java -server -Xms2G -Xmx2G -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -XX:ShenandoahGCMode=iu -XX:+UseNUMA -XX:+AlwaysPreTouch -XX:-UseBiasedLocking -XX:+DisableExplicitGC -jar server.jar nogui
+ echo "СУКА БЛЯТЬ Я ТВОЙ РОТ ЕБАЛ!"
+            echo "Рестарт нахуй через:"
+            for i in 5 4 3 2 1
+            do
+               echo "$i..."
+               sleep 1
+            done
+            echo "Ты пидорас!"
+         done
